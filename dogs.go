a84dd2ba06071d3c872076ae132ad178fa30c879
Ritvik Rohan <ritvik.rohan96@gmail.com>
2023-11-20 22:43:08 +0530

    Second commit

---
+++ dogs.go
@@ -0,0 +1,7 @@
+package dogs
+
+import "strings"
+
+func WhenGrowsUp(s string) string {
+	return "When grows up will:" + strings.ToUpper(s)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ritvikrohan/dogs
+
+go 1.21.4
